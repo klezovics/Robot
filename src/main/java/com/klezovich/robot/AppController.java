f6368800299592f6f6cf058887de745d70c88207
Arturs Klezovics <klezovich@gmail.com>
2018-11-26 19:19:50 +0000

    Add endpoint for the front-end to query when it needs scenarion exec.

--- src/main/java/com/klezovich/robot/AppController.java
+++ src/main/java/com/klezovich/robot/AppController.java
@@ -4,9 +4,12 @@ import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AppController {
@@ -22,4 +25,14 @@ public class AppController {
 		//System.out.println(script);
 		return "index";
 	}
+	
+	@PostMapping(value="/robots")
+	@ResponseBody
+	public Coordinates getRobotMovements( @RequestBody String str , ModelMap m ) {
+		System.out.println(m);
+		System.out.println("String is:" + str);
+		return new Coordinates(0,0,Orientation.WEST);
+	}
+	
+	
 }
