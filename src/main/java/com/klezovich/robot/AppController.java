bb0c8fe57d1804a36afbd44fc371d81195766fe0
Arturs Klezovics <klezovich@gmail.com>
2018-11-26 17:25:07 +0000

    Add mapping for receipt of scripts from front-end

--- src/main/java/com/klezovich/robot/AppController.java
+++ src/main/java/com/klezovich/robot/AppController.java
@@ -1,13 +1,25 @@
 package com.klezovich.robot;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
 
 	@GetMapping("/")
-	public String getHomepage() {
+	public String getHomepage( Model m ) {
+		m.addAttribute("script", new Script() );
+		return "index";
+	}
+	
+	@PostMapping("/")
+	public String processScript( @Valid @ModelAttribute("script") Script script, Model m ) {
+		System.out.println(script);
 		return "index";
 	}
 }
