810be7b29576e3f4eb1e9b8879d2aabd85150d12
Arturs Klezovics <klezovich@gmail.com>
2018-11-26 17:33:13 +0000

    Set access modifiers and add constructor

--- src/main/java/com/klezovich/robot/Coordinates.java
+++ src/main/java/com/klezovich/robot/Coordinates.java
@@ -1,10 +1,18 @@
 package com.klezovich.robot;
 
-public class RobotCoordinates {
+public class Coordinates {
 
-	Orientation orienation;
-    Integer X;
-    Integer Y;
+	private Integer x;
+    private  Integer y;
+	private Orientation orienation;
+    
+	public Coordinates( Integer x, Integer y, Orientation orientation ) {
+		this.x=x;
+		this.y=y;
+		this.orienation = orientation;
+	}
+	
+    
 	public Orientation getOrienation() {
 		return orienation;
 	}
@@ -12,16 +20,16 @@ public class RobotCoordinates {
 		this.orienation = orienation;
 	}
 	public Integer getX() {
-		return X;
+		return x;
 	}
 	public void setX(Integer x) {
-		X = x;
+		this.x = x;
 	}
 	public Integer getY() {
-		return Y;
+		return y;
 	}
 	public void setY(Integer y) {
-		Y = y;
+		this.y = y;
 	}
     
     
