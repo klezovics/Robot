e324f95c767cd46eb42a2861ea7e77f6edbd322a
Arturs Klezovics <klezovich@gmail.com>
2018-11-26 18:01:10 +0000

    Define 'Robot' class interface

--- src/main/java/com/klezovich/robot/Robot.java
+++ src/main/java/com/klezovich/robot/Robot.java
@@ -8,6 +8,26 @@ public class Robot {
 		this.coordinates = coordinates;
 	}
 	
+	public Coordinates setPosition( Coordinates coordinates ) {
+		return coordinates;
+	}
+	
+	public Coordinates move( Integer distance, Direction direction ) {
+		return coordinates;
+	}
+	
+	public Coordinates rotate( Direction direction ) {
+		return coordinates;
+	}
+	
+	public Coordinates turnAround() {
+		return coordinates;
+	}
+
+	public void sleep() {
+		return;
+	}
+	
 	
 	
 }
