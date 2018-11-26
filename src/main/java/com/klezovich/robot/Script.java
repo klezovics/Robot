bb6a23f6691c5885ffa42754ec968e705859785d
Arturs Klezovics <klezovich@gmail.com>
2018-11-26 17:24:50 +0000

    Add class to encapsulate scripts received from front-end

---
+++ src/main/java/com/klezovich/robot/Script.java
@@ -0,0 +1,20 @@
+package com.klezovich.robot;
+
+public class Script {
+
+	String text;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public String toString() {
+		return text;
+	}
+	
+}
