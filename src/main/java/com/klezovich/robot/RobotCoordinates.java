6cc82207b539ce36706bd2de40f11b347f4fba53
Arturs Klezovics <klezovich@gmail.com>
2018-11-26 17:28:48 +0000

    Add class to encapsulate robot coordinates

---
+++ src/main/java/com/klezovich/robot/RobotCoordinates.java
@@ -0,0 +1,30 @@
+package com.klezovich.robot;
+
+public class RobotCoordinates {
+
+	Orientation orienation;
+    Integer X;
+    Integer Y;
+	public Orientation getOrienation() {
+		return orienation;
+	}
+	public void setOrienation(Orientation orienation) {
+		this.orienation = orienation;
+	}
+	public Integer getX() {
+		return X;
+	}
+	public void setX(Integer x) {
+		X = x;
+	}
+	public Integer getY() {
+		return Y;
+	}
+	public void setY(Integer y) {
+		Y = y;
+	}
+    
+    
+    
+    
+}
