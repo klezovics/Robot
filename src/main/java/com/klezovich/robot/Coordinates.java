50b8a1791413d051496b0c46c0a083db6fdd8cb0
Arturs Klezovics <klezovich@gmail.com>
2018-11-26 17:36:29 +0000

    Minor fix - change spacing

--- src/main/java/com/klezovich/robot/Coordinates.java
+++ src/main/java/com/klezovich/robot/Coordinates.java
@@ -3,36 +3,37 @@ package com.klezovich.robot;
 public class Coordinates {
 
 	private Integer x;
-    private Integer y;
+	private Integer y;
 	private Direction direction;
-    
-	public Coordinates( Integer x, Integer y, Direction direction ) {
-		this.x=x;
-		this.y=y;
+
+	public Coordinates(Integer x, Integer y, Direction direction) {
+		this.x = x;
+		this.y = y;
 		this.direction = direction;
 	}
-	
-    
+
 	public Direction getDirection() {
 		return direction;
 	}
+
 	public void setdirection(Direction direction) {
 		this.direction = direction;
 	}
+
 	public Integer getX() {
 		return x;
 	}
+
 	public void setX(Integer x) {
 		this.x = x;
 	}
+
 	public Integer getY() {
 		return y;
 	}
+
 	public void setY(Integer y) {
 		this.y = y;
 	}
-    
-    
-    
-    
+
 }
