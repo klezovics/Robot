4047d28b2cf803139f1b11a5c18cdfd8592e1319
Arturs Klezovics <klezovich@gmail.com>
2018-11-30 18:13:22 +0000

    Refactor the grid to be a class

--- src/main/resources/static/draw_robot_page.js
+++ src/main/resources/static/draw_robot_page.js
@@ -1,7 +1,5 @@
 var maxH = 5;
 var maxW = 5;
-drawGrid(maxH, maxW);
 
-
-
+Grid.draw(maxH, maxW);
 Robot.draw("grid_cell_0x0");
--- src/main/resources/static/grid_lib.js
+++ src/main/resources/static/grid_lib.js
@@ -1,16 +1,23 @@
-function drawGrid(maxH, maxW) {
 
-	var table = document.createElement("table");
-	for (var i = 0; i < maxH; i++) {
-		var tr = document.createElement('tr');
-		for (var j = 0; j < maxW; j++) {
-			var td = document.createElement('td');
-			td.id = "grid_cell_"+i + "x" + j;
-			
-			tr.appendChild(td);
-		}
-		table.appendChild(tr);
-	}
+class Grid{
+	
+   static draw( maxH, maxW ){
+	 		
+	 var table = document.createElement("table");
+	 for (var i = 0; i < maxH; i++) {
+		 
+	   var tr = document.createElement('tr');
+	   for (var j = 0; j < maxW; j++) {
+	     var td = document.createElement('td');
+		 td.id = "grid_cell_"+i + "x" + j;			
+					tr.appendChild(td);
+	   }
+	   table.appendChild(tr);
+	 }
 
-	document.getElementById("grid_container").appendChild(table);
+	 document.getElementById("grid_container").appendChild(table);
+   }
+   
+	
 }
+
