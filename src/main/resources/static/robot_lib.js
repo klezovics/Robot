bce35ef516b8034f4c547a50e3589f31c3dc7d7f
Arturs Klezovics <klezovich@gmail.com>
2018-11-30 17:12:50 +0000

    Make Robot a class

--- src/main/resources/static/robot_lib.js
+++ src/main/resources/static/robot_lib.js
@@ -1,3 +1,27 @@
+
+
+class Robot{
+	
+	constructor(){
+		var robot = document.createElement(img);
+		robot.id="robot";
+		robot.width="50px";
+		robot.height="50px";
+		src="Robot.png";
+		this._robot = robot
+	};
+	
+	draw( squareId ){	   	
+		$("#"+squareId).appendChild(robot);
+	}
+	
+	static remove(){
+	   this._robot.remove();	
+	}
+	
+	
+}
+
 function drawRobot(squareId) {
 	document.getElementById(squareId).innerHTML = '<img src="Robot.png" id="robot" width="50px" height="50px" />';
 }
