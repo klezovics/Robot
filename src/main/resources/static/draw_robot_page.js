94afd0c766f4b1ccb599626e03bff757e29f9746
Arturs Klezovics <klezovich@gmail.com>
2018-11-30 18:45:12 +0000

    Minor fix

--- src/main/resources/static/draw_robot_page.js
+++ src/main/resources/static/draw_robot_page.js
@@ -2,7 +2,7 @@ var maxH = 5;
 var maxW = 5;
 Grid.draw(maxH, maxW);
 
-Robot.draw("grid_cell_0x0");
+Robot.draw(0,0);
 
 $("#reset").click( clearRobotData );
 $("#submit").click( BackEndCommunicationManager.processUserScript );
