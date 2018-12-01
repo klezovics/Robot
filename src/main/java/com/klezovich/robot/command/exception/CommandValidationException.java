2da08584699ef93f45a6ebdb444c66c94172c22a
Arturs Klezovics <klezovich@gmail.com>
2018-12-01 22:00:07 +0000

    Refactor command validation exception

--- src/main/java/com/klezovich/robot/command/exception/CommandValidationException.java
+++ src/main/java/com/klezovich/robot/command/exception/CommandValidationException.java
@@ -4,14 +4,19 @@ package com.klezovich.robot.command.exception;
 public class CommandValidationException extends RuntimeException {
  
 	String error; 
+	String cmdTag="";
 	
 	public CommandValidationException( String error ){
 		this.error = error;
 	}
 
+	public void setCmdTag( String cmdTag ) {
+		this.cmdTag = cmdTag;
+	}
+	
 	@Override
 	public String toString() {
-		return "CommandValidationException:"+error;
+		return "Command " + cmdTag+":"+error;
 	}	
 	
 }
