936c554cc0f55cc93c22b2c410644d9e1328054a
Arturs Klezovics <klezovich@gmail.com>
2018-12-01 21:40:24 +0000

    Add argument definitions

--- src/main/java/com/klezovich/robot/command/PositionCommand.java
+++ src/main/java/com/klezovich/robot/command/PositionCommand.java
@@ -1,11 +1,19 @@
 package com.klezovich.robot.command;
 
+import com.klezovich.robot.Orientation;
 import com.klezovich.robot.Robot;
 
 public class PositionCommand extends Command {
 
 	private static final String tag = "POSITION";
 	private String[] args;
+	
+	// Describing argument list
+	static {
+		addArgumentDefinition(0, Integer.class );
+		addArgumentDefinition(1, Integer.class );
+		addArgumentDefinition(2, Orientation.class );
+	}
 	
 	public PositionCommand( String[] args ) {
 		super(args);
