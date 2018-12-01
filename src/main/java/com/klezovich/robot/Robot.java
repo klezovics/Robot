4d2ed5e60c0020afa734915887b9ed516fcf7522
Arturs Klezovics <klezovich@gmail.com>
2018-12-01 22:13:09 +0000

    Implement the moveForward() method

--- src/main/java/com/klezovich/robot/Robot.java
+++ src/main/java/com/klezovich/robot/Robot.java
@@ -16,6 +16,31 @@ public class Robot {
 		return coordinates;
 	}
 	
+	public Coordinates moveForward( Integer distance ) {
+		
+		Orientation currentOrientation = coordinates.getOrientation();
+		int x = coordinates.getX();
+		int y = coordinates.getY();
+		
+		switch( currentOrientation ) {
+		   case NORTH:
+			   coordinates.setY(x - distance);
+			   break;
+		   case SOUTH:
+			   coordinates.setY(y + distance);
+			   break;
+		   case WEST:
+			   coordinates.setX( x - distance );
+			   break;
+		   case EAST:
+			   coordinates.setX( x + distance );
+			   break;
+		}
+		
+		return coordinates;
+	}
+	
+	
 	public Coordinates rotate( Direction direction ) {
 		return coordinates;
 	}
