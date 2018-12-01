5725755b8724ba03b302d46f95c8b65de089a873
Arturs Klezovics <klezovich@gmail.com>
2018-12-01 22:42:53 +0000

    Add method to calculate next orientation in case of rotation

--- src/main/java/com/klezovich/robot/Orientation.java
+++ src/main/java/com/klezovich/robot/Orientation.java
@@ -1,5 +1,53 @@
 package com.klezovich.robot;
 
+import static com.klezovich.robot.Direction.LEFT;
+import static com.klezovich.robot.Direction.RIGHT;
+import static com.klezovich.robot.Orientation.EAST;
+import static com.klezovich.robot.Orientation.NORTH;
+import static com.klezovich.robot.Orientation.SOUTH;
+import static com.klezovich.robot.Orientation.WEST;
+
 public enum Orientation {
-  WEST,EAST,NORTH,SOUTH;
+	WEST, EAST, NORTH, SOUTH;
+
+	Orientation rotate(Direction rotationDir) {
+
+		if (rotationDir.equals(LEFT)) {
+
+			switch (this) {
+			case NORTH:
+				return WEST;
+
+			case SOUTH:
+				return EAST;
+
+			case EAST:
+				return NORTH;
+
+			case WEST:
+				return SOUTH;
+
+			}
+		}
+
+		if (rotationDir.equals(RIGHT)) {
+
+			switch (this) {
+			case NORTH:
+				return EAST;
+
+			case SOUTH:
+				return WEST;
+
+			case EAST:
+				return SOUTH;
+
+			case WEST:
+				return NORTH;
+
+			}
+		}
+
+		return null;
+	}
 }
