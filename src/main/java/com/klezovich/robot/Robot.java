3c29bcb3dfc21589097949930d0d1ccba4ca2e7a
Arturs Klezovics <klezovich@gmail.com>
2018-12-01 22:44:39 +0000

    Implement some of the methods

--- src/main/java/com/klezovich/robot/Robot.java
+++ src/main/java/com/klezovich/robot/Robot.java
@@ -1,50 +1,63 @@
 package com.klezovich.robot;
 
+import static com.klezovich.robot.Orientation.*;
+import static com.klezovich.robot.Direction.*;
+
 public class Robot {
 
 	Coordinates coordinates;
+
+	public Robot() {
+		
+	}
 	
-	public Robot( Coordinates coordinates ) {
+	public Robot(Coordinates coordinates) {
 		this.coordinates = coordinates;
 	}
-	
-	public Coordinates setPosition( Coordinates coordinates ) {
+
+	public Coordinates setPosition(Coordinates coordinates) {
+		this.coordinates = coordinates;
 		return coordinates;
 	}
-	
-	public Coordinates move( Integer distance, Direction direction ) {
+
+	public Coordinates move(Integer distance, Direction direction) {
 		return coordinates;
 	}
-	
-	public Coordinates moveForward( Integer distance ) {
-		
+
+	public Coordinates moveForward(Integer distance) {
+
 		Orientation currentOrientation = coordinates.getOrientation();
 		int x = coordinates.getX();
 		int y = coordinates.getY();
-		
-		switch( currentOrientation ) {
-		   case NORTH:
-			   coordinates.setY(x - distance);
-			   break;
-		   case SOUTH:
-			   coordinates.setY(y + distance);
-			   break;
-		   case WEST:
-			   coordinates.setX( x - distance );
-			   break;
-		   case EAST:
-			   coordinates.setX( x + distance );
-			   break;
+
+		switch (currentOrientation) {
+		case NORTH:
+			coordinates.setY(x - distance);
+			break;
+		case SOUTH:
+			coordinates.setY(y + distance);
+			break;
+		case WEST:
+			coordinates.setX(x - distance);
+			break;
+		case EAST:
+			coordinates.setX(x + distance);
+			break;
 		}
-		
+
 		return coordinates;
 	}
-	
-	
-	public Coordinates rotate( Direction direction ) {
+
+	public Coordinates rotate(Direction direction) {
+
+		Orientation robotOrientation = coordinates.getOrientation();
+        robotOrientation = robotOrientation.rotate(direction);
+	    coordinates.setOrientation(robotOrientation);
+
 		return coordinates;
+
 	}
-	
+
 	public Coordinates turnAround() {
 		return coordinates;
 	}
@@ -52,7 +65,5 @@ public class Robot {
 	public void sleep() {
 		return;
 	}
-	
-	
-	
+
 }
