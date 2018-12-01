1c140e9e0cdcef3c39870182104b56a892660880
Arturs Klezovics <klezovich@gmail.com>
2018-12-01 23:50:55 +0000

    Add more tests

--- src/test/java/com/klezovich/robot/Robot_Test.java
+++ src/test/java/com/klezovich/robot/Robot_Test.java
@@ -14,95 +14,138 @@ public class Robot_Test {
 
 	@Test
 	public void test_Constructors() {
-		
-		Coordinates c = new Coordinates(0,0,NORTH);
+
+		Coordinates c = new Coordinates(0, 0, NORTH);
 		Robot r = new Robot(c);
-		assertEquals(c, r.getCoordinates() );
-	
+		assertEquals(c, r.getCoordinates());
+
 		r = new Robot();
 		r.setCoordinates(c);
-		assertEquals(c, r.getCoordinates() );
-		
+		assertEquals(c, r.getCoordinates());
+
 	}
-	
+
 	@Test
 	public void test_Rotations() {
-	
+
 		Robot r = new Robot();
-		Coordinates c = new Coordinates(0,0,NORTH);
-		
+		Coordinates c = new Coordinates(0, 0, NORTH);
+
 		r.setCoordinates(c);
 		r.rotate(LEFT);
-		assertEquals( new Coordinates(0,0,WEST) , r.getCoordinates() );
+		assertEquals(new Coordinates(0, 0, WEST), r.getCoordinates());
 		r.rotate(LEFT);
-		assertEquals( new Coordinates(0,0,SOUTH) , r.getCoordinates() );
+		assertEquals(new Coordinates(0, 0, SOUTH), r.getCoordinates());
 		r.rotate(LEFT);
-		assertEquals( new Coordinates(0,0,EAST) , r.getCoordinates() );
+		assertEquals(new Coordinates(0, 0, EAST), r.getCoordinates());
 		r.rotate(LEFT);
-		assertEquals( new Coordinates(0,0,NORTH) , r.getCoordinates() );
-	    
-		
+		assertEquals(new Coordinates(0, 0, NORTH), r.getCoordinates());
+
 		r.setCoordinates(c);
 		r.rotate(RIGHT);
-		assertEquals( new Coordinates(0,0,EAST) , r.getCoordinates() );
+		assertEquals(new Coordinates(0, 0, EAST), r.getCoordinates());
 		r.rotate(RIGHT);
-		assertEquals( new Coordinates(0,0,SOUTH) , r.getCoordinates() );
+		assertEquals(new Coordinates(0, 0, SOUTH), r.getCoordinates());
 		r.rotate(RIGHT);
-		assertEquals( new Coordinates(0,0,WEST) , r.getCoordinates() );
+		assertEquals(new Coordinates(0, 0, WEST), r.getCoordinates());
 		r.rotate(RIGHT);
-		assertEquals( new Coordinates(0,0,NORTH) , r.getCoordinates() );
-	
+		assertEquals(new Coordinates(0, 0, NORTH), r.getCoordinates());
+
 		r.setCoordinates(c);
 		r.rotate(RIGHT);
-		assertEquals( new Coordinates(0,0,EAST) , r.getCoordinates() );
+		assertEquals(new Coordinates(0, 0, EAST), r.getCoordinates());
 		r.rotate(LEFT);
-		assertEquals( new Coordinates(0,0,NORTH) , r.getCoordinates() );
+		assertEquals(new Coordinates(0, 0, NORTH), r.getCoordinates());
 		r.rotate(RIGHT);
-		assertEquals( new Coordinates(0,0,EAST) , r.getCoordinates() );
+		assertEquals(new Coordinates(0, 0, EAST), r.getCoordinates());
 		r.rotate(RIGHT);
-	
+
 	}
-	
+
 	@Test
 	public void test_LinearMovements() {
-		
+
 		Robot r = new Robot();
-		
+
 		// Moving south tests
-		Coordinates c = new Coordinates(0,0,SOUTH);
-		
+		Coordinates c = new Coordinates(0, 0, SOUTH);
+
 		r.setCoordinates(c);
 		r.moveForward(1);
-		assertEquals(r.getCoordinates(),  new Coordinates(0,1,SOUTH) );
-		
+		assertEquals(r.getCoordinates(), new Coordinates(0, 1, SOUTH));
+
 		r.moveForward(3);
-		assertEquals(r.getCoordinates(),  new Coordinates(0,4,SOUTH) );
-		
-		
+		assertEquals(r.getCoordinates(), new Coordinates(0, 4, SOUTH));
+
 		// Moving north tests
-		c = new Coordinates(2,4, NORTH);
+		c = new Coordinates(2, 4, NORTH);
 		r.setCoordinates(c);
 		r.moveForward(1);
-		assertEquals( new Coordinates(2,3, NORTH), r.getCoordinates()  );
+		assertEquals(new Coordinates(2, 3, NORTH), r.getCoordinates());
 		r.moveForward(3);
-		assertEquals(  new Coordinates(2,0, NORTH), r.getCoordinates() );
-		
-		// Moving east tests 
-		c = new Coordinates(1,4, EAST);
+		assertEquals(new Coordinates(2, 0, NORTH), r.getCoordinates());
+
+		// Moving east tests
+		c = new Coordinates(1, 4, EAST);
 		r.setCoordinates(c);
 		r.moveForward(1);
-		assertEquals( new Coordinates(2,4, EAST), r.getCoordinates()  );
+		assertEquals(new Coordinates(2, 4, EAST), r.getCoordinates());
 		r.moveForward(2);
-		assertEquals(  new Coordinates(4,4, EAST), r.getCoordinates() );
-		
+		assertEquals(new Coordinates(4, 4, EAST), r.getCoordinates());
+
 		// Moving west tests
-		c = new Coordinates(5,4, WEST);
+		c = new Coordinates(5, 4, WEST);
 		r.setCoordinates(c);
 		r.moveForward(1);
-		assertEquals( new Coordinates(4,4, WEST), r.getCoordinates()  );
+		assertEquals(new Coordinates(4, 4, WEST), r.getCoordinates());
 		r.moveForward(2);
-		assertEquals(  new Coordinates(2,4, WEST), r.getCoordinates() );
-		
+		assertEquals(new Coordinates(2, 4, WEST), r.getCoordinates());
+
 	}
-	
+
+	@Test
+	public void test_CompountMovements1() {
+
+		Robot r = new Robot();
+
+		Coordinates c = new Coordinates(0, 0, SOUTH);
+		r.setCoordinates(c);
+		r.moveForward(4);
+		assertEquals(new Coordinates(0, 4, SOUTH), r.getCoordinates());
+
+		r.rotate(Direction.LEFT);
+		assertEquals(new Coordinates(0, 4, EAST), r.getCoordinates());
+
+		r.moveForward(2);
+		assertEquals(new Coordinates(2, 4, EAST), r.getCoordinates());
+
+		r.rotate(Direction.LEFT);
+		r.rotate(Direction.LEFT);
+		r.moveForward(2);
+		assertEquals(new Coordinates(0, 4, WEST), r.getCoordinates());
+
+		r.rotate(Direction.RIGHT);
+		r.moveForward(3);
+		assertEquals(new Coordinates(0, 1, NORTH), r.getCoordinates());
+
+	}
+
+	@Test
+	public void test_TraverseArenaClockwise() {
+		Robot r = new Robot();
+
+		Coordinates c = new Coordinates(0, 0, SOUTH);
+        r.setCoordinates(c);
+		
+        r.moveForward(5);
+        r.rotate(LEFT);
+        r.moveForward(5);
+        r.rotate(LEFT);
+        r.moveForward(5);
+        r.rotate(LEFT);
+        r.moveForward(5);
+        
+		assertEquals( new Coordinates(0,0,WEST), r.getCoordinates() );
+	}
+
 }
