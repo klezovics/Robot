1921988ad6f8762e68b9ccdeff9f3aba6bf051b6
Arturs Klezovics <klezovich@gmail.com>
2018-12-02 23:17:19 +0000

    Split the controller functionality in two

--- src/main/java/com/klezovich/robot/controller/BackEndController.java
+++ src/main/java/com/klezovich/robot/controller/BackEndController.java
@@ -1,57 +1,21 @@
 package com.klezovich.robot.controller;
 
-import java.security.Principal;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.klezovich.robot.domain.Robot;
-import com.klezovich.robot.domain.Script;
 import com.klezovich.robot.domain.command.Command;
 import com.klezovich.robot.domain.command.CommandParser;
 import com.klezovich.robot.domain.command.exception.ScriptExecutionException;
 import com.klezovich.robot.json.JsonErrorDto;
 
 @Controller
-public class AppController {
-
-	@GetMapping("/")
-	public String start() {
-		return "login";
-	}
-	
-	@GetMapping("/login")
-	public String getLogin() {
-		return "login";
-	}
-
-	/*@PostMapping("/login")
-	public String processLogin( Model m, Principal p ) {
-		m.addAttribute("script", new Script());
-
-		if (p != null)
-			m.addAttribute("userName", p.getName());
-		else
-			m.addAttribute("userName", "Anonymous");
-		return "robot_control_page";
-	}*/
-
-	@GetMapping("/robot_control_page")
-	public String getRobotControlPage(Model m, Principal p) {
-		m.addAttribute("script", new Script());
-
-		if (p != null)
-			m.addAttribute("userName", p.getName());
-		else
-			m.addAttribute("userName", "Anonymous");
-		return "robot_control_page";
-	}
+public class BackEndController {
 
 	@PostMapping(value = "/robots/")
 	@ResponseBody
@@ -87,5 +51,4 @@ public class AppController {
 
 		return r.getCoordinates();
 	}
-
 }
---
+++ src/main/java/com/klezovich/robot/controller/FrontEndController.java
@@ -0,0 +1,48 @@
+package com.klezovich.robot.controller;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.klezovich.robot.domain.Robot;
+import com.klezovich.robot.domain.Script;
+import com.klezovich.robot.domain.command.Command;
+import com.klezovich.robot.domain.command.CommandParser;
+import com.klezovich.robot.domain.command.exception.ScriptExecutionException;
+import com.klezovich.robot.json.JsonErrorDto;
+
+@Controller
+public class FrontEndController {
+
+	@GetMapping("/")
+	public String start() {
+		return "login";
+	}
+	
+	@GetMapping("/login")
+	public String getLogin() {
+		return "login";
+	}
+
+	
+	@GetMapping("/robot_control_page")
+	public String getRobotControlPage(Model m, Principal p) {
+		m.addAttribute("script", new Script());
+
+		if (p != null)
+			m.addAttribute("userName", p.getName());
+		else
+			m.addAttribute("userName", "Anonymous");
+		return "robot_control_page";
+	}
+
+
+
+}
