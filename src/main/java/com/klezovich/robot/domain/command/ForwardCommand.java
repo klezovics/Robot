dbf0ed9c68d0dfc4e685aea98fca437fbf08a720
Arturs Klezovics <klezovich@gmail.com>
2018-12-04 14:35:57 +0000

    Remove unnecessary method

--- src/main/java/com/klezovich/robot/domain/command/ForwardCommand.java
+++ src/main/java/com/klezovich/robot/domain/command/ForwardCommand.java
@@ -6,52 +6,45 @@ import com.klezovich.robot.domain.command.exception.CommandValidationException;
 import com.klezovich.robot.domain.command.exception.ScriptExecutionException;
 
 public class ForwardCommand extends Command {
-	
+
 	private Integer distance;
-	
-	public ForwardCommand( String[] args) {
+
+	public ForwardCommand(String[] args) {
 		super(args);
 		this.name = "FORWARD";
 		validate();
 	}
-	
+
 	@Override
 	public Coordinates execute(Robot r) {
-	
-	 try {
-	    return r.moveForward( distance );	
-	 }catch( Robot.RobotControlException e ) {
-		 throw new ScriptExecutionException(this, e.getMessage() );
-	 }
+
+		try {
+			return r.moveForward(distance);
+		} catch (Robot.RobotControlException e) {
+			throw new ScriptExecutionException(this, e.getMessage());
+		}
 	}
 
 	@Override
 	protected boolean validate() {
-		
-		if( args.length != 1 )
-			throw new CommandValidationException(this,"command expects one argument");
-		
+
+		if (args.length != 1)
+			throw new CommandValidationException(this, "command expects one argument");
+
 		Integer distance = null;
 		try {
 			distance = Integer.parseInt(args[0]);
-		}catch( NumberFormatException nfe ) {
+		} catch (NumberFormatException nfe) {
 			throw new CommandValidationException(this, "first argument must be an integer");
 		}
 		
-		if( distance < 0 )
+
+		if (distance < 0)
 			throw new CommandValidationException(this, "first argument must be non-negative");
-		
-		initializeFields();
-		return true;
-	}
-	
-	
-	private boolean initializeFields() {
-		
-		
-		distance = Integer.valueOf(args[0]);
+
+		this.distance = distance;
 		
 		return true;
 	}
-	
+
 }
